f0385885cdfa79b5189cd6a0fabb61864a88951d
dowglasOficial <bittencourtdouglas41@gmail.com>
2025-05-19 19:13:50 -0300

    arquivos default do spring

---
+++ ESList/src/main/java/com/dowglas/ESList/EsListApplication.java
@@ -0,0 +1,13 @@
+package com.dowglas.ESList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsListApplication.class, args);
+	}
+
+}
---
+++ ESList/src/test/java/com/dowglas/ESList/EsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dowglas.ESList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
